c8b6105fd5fcc35f39e0cda72750a4454100b51a
Douman <douman@gmx.se>
2016-07-30 11:35:37 +0300

    Travis CI

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Devil Survivor 2 RB Save Editor
 
-WIP
+[![Build Status](https://travis-ci.org/DoumanAsh/ds-js.svg?branch=master)](https://travis-ci.org/DoumanAsh/ds-js)
+
+This is a online save editor for Devil Survivor 2 Record Breaker.
+
+Following can be modified:
+- Macca
---
+++ ci/update_page.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+REPO=`git config remote.origin.url`
+SHA=`git log -1 --format="%s(%h %cd)" --date=short`
+
+git clone $REPO out/
+
+ls
+
+cd out/
+
+git checkout gh-pages
+rm -rf *
+
+cp -rf ../public/* .
+
+git status
+if [[ -z `git status --porcelain` ]]; then
+    echo "No changes to the site"
+    return 0
+fi
+
+git config user.name "Travis CI"
+git config user.email "$COMMIT_AUTHOR_EMAIL"
+echo "https://${GIT_TOKEN}:x-oauth-basic@github.com\n" > ~/.git-credentials
+git config remote.origin.url "https://${GIT_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git"
+
+git add --all .
+git commit -m "Auto-update" -m "Commit: ${SHA}"
+git push origin HEAD
